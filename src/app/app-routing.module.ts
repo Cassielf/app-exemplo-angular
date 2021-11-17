4a72a33523614c9ef1b5d9e6a7de85cdd0bb8cf6
byanca <byanca.calixto@dcx.ufpb.br>
2021-11-17 16:01:01 -0300

    ajuste navbar e logo, configuracao de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdicionarComponent } from './Model/Pessoa/adicionar/adicionar.component';
+import { EditarComponent } from './Model/Pessoa/editar/editar.component';
+import { ListarComponent } from './Model/Pessoa/listar/listar.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'listar', component: ListarComponent},
+  {path: 'editar', component: EditarComponent},
+  {path: 'adicionar', component: AdicionarComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
